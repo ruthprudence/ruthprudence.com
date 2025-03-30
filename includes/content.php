fc95981993f71d99c42b9f0eb186dd86afa04ed2
Ruth Prudence <ruth@ruthprudence.com>
2025-03-29 21:48:06 -0700

    after rebase

---
+++ includes/content.php
@@ -0,0 +1,12 @@
+<h1>Web 170 Staging Area</h1>
+
+<section>
+<ul>
+  <li><h2>Description</h2></li><p>My Web 170 project is my personal profile page, different from my professional portfolio (<a href="http://ruthmansoor.com" target="blank">RuthMansoor.com</a>). RuthPrudence.com will host my essays, photos, and act as my blogging site, (eventually I want to run for government office, so this site will serve as a gradual platform-building and revealing of opinion).</p>
+  <li><h2>Protosite</h2></li><p><a href="http://ruthprudence.com/" target="blank">http://ruthprudence.com/</a></p>
+  <li><h2>Templates</h2></li><p>Website: <a href="  http://ruthprudence.com/templates/" target="blank">Templates</a></p>
+  <li><h2>Protosite (Theme) Repository</h2></li><p><a href="https://github.com/ruthproof/web170" target="blank">GitHub</a></p>
+
+</ul>
+
+</section>
---
+++ includes/top.php
@@ -0,0 +1,20 @@
+<?php
+
+echo '
+  <DOCTYPE! html>
+  <html>
+      <head>
+      <title id="main">ruthprudence.com</title>
+
+      <meta charset="utf-8"></meta>
+
+  	  <meta contents="width=device-width, initial-scale-1" name="viewport"></meta>
+
+
+      <meta name="robots" content="noindex,nofollow"/>
+
+	    <link rel="stylesheet" type="text/css" href="./includes/styles.css"></link>
+      </head>
+
+  <body>
+';
